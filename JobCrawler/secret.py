144a944e565d003086bdcc44b2b18e445371549d
Li Zhao <iloahz@gmail.com>
2013-12-17 16:19:51 +0800

    move stuff to secret

---
+++ JobCrawler/secret.py
@@ -0,0 +1,8 @@
+__author__ = 'iloahz'
+
+SECRET_KEY = 'n(+t47%zsxnok%@pk!#)&39q#-9_d9uxhxv6njaoc8qt)4unvh'
+DB_NAME = 'jobcrawler'
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'asdf1234'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
