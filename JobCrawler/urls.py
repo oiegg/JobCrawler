baf36085aaaac70708ee8e69e5156811b159d1e4
Li Zhao <iloahz@gmail.com>
2013-12-18 14:02:18 +0800

    handle static

--- JobCrawler/urls.py
+++ JobCrawler/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,3 +14,5 @@ urlpatterns = patterns('',
     url(r'^poster', include('poster.urls')),
     url(r'^inspector', include('inspector.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
